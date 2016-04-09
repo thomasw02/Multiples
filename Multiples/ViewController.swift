a33914b815fe4da6c8c17b3998db35b63063230a
Thomas Woodfield <thomas.w02@icloud.com>
2016-04-09 15:01:25 +1200

    First Commit
    W.I.P

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -9,17 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
+    @IBOutlet weak var mainTitle: UIImageView!
+    @IBOutlet weak var enterMultiple: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    
 }
 
